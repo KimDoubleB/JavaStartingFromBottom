397c05b59907694196423b42a2a99fc2b27355b9
KimDoubleB <tree9295@gmail.com>
2020-12-16 23:10:39 +0900

    [UPDATE] builder practices using chaining

--- DesignPattern/BuilderPattern/src/BuilderPattern.java
+++ DesignPattern/BuilderPattern/src/BuilderPattern.java
@@ -7,6 +7,8 @@ public class BuilderPattern {
      * 동일한 컴퓨터 생성 메서드(make)로 청사진에 맞는 컴퓨터를 만들어내고, 그 컴퓨터 객체를 가져올 수 있다.
      *
      * 비슷한 객체를 조금의 설정을 달리하여 많이 생성해야하는 경우, 유용하게 사용할 수 있을 것 같다.
+     * 또한, 많은 변수를 가진 객체를 생성할 때, 편리하고 가독성있게 구현할 수 있다.
+     *
      */
 
     public static void main(String[] args) {
@@ -15,17 +17,26 @@ public class BuilderPattern {
         factory.setBluePrint(new MacBookBluePrint());
         factory.make();
 
-        Computer Macbook = factory.getComputer();
+        Computer macBook = factory.getComputer();
 
-        System.out.println(Macbook.toString());
-        System.out.println(Macbook.hashCode());
+        System.out.println(macBook.toString());
+        System.out.println(macBook.hashCode());
 
         factory.setBluePrint(new LgGramBluePrint());
         factory.make();
-        Computer Macbook2 = factory.getComputer();
-        System.out.println(Macbook2.toString());
-        System.out.println(Macbook2.hashCode());
+        Computer lgGram = factory.getComputer();
+        System.out.println(lgGram.toString());
+        System.out.println(lgGram.hashCode());
 
 
+        // Use chaining
+        Computer macBook2 = ComputerBuilder
+                .start()
+                .setCpu("i7")
+                .setRam("128g")
+                .setStorage("ssd 512g")
+                .build();
+
+        System.out.println(macBook2.toString());
     }
 }
